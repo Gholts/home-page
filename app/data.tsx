10b4c85ac14ba88888c6b8b53d567aa0f339e3ee
Gholts <104642670+Gholts@users.noreply.github.com>
2024-05-06 07:08:27 +0800

    Update data.tsx

--- app/data.tsx
+++ app/data.tsx
@@ -1,10 +1,7 @@
 import { MailIcon, MapPinIcon } from "lucide-react";
 import { nanoid } from 'nanoid'
 import { GitHubIcon } from "@/components/icons/github-icon";
-import { JuejinIcon } from "@/components/icons/juejin-icon";
 import { AllCard } from "@/app/cards";
-import ClockCard from "@/app/cards/clock";
-import MoodCard from "@/app/cards/mood";
 
 export const carouselList: Array<AllCard> = [
   {
@@ -13,9 +10,9 @@ export const carouselList: Array<AllCard> = [
     previewImg: '/images/no-image.png',
     name: 'Empty',
     tags: [
-      { name: 'HTML', color: '#E34C26' },
-      { name: 'CSS', color: '#563D7C' },
-      { name: 'JavaScript', color: '#F1E05A' },
+      { name: 'There', color: '#E34C26' },
+      { name: 'is', color: '#563D7C' },
+      { name: 'nothing', color: '#F1E05A' },
     ],
     describe: 'Nothing, It is placeholder',
     codeUrl: '',
@@ -24,54 +21,25 @@ export const carouselList: Array<AllCard> = [
   {
     id: nanoid(),
     type: 'project',
-    previewImg: '/images/barrage-preview.png',
-    name: 'Web Barrage',
-    tags: [
-      { name: 'TypeScript', color: '#3178C6' },
-      { name: 'JavaScript', color: '#F1E05A' },
-      { name: 'HTML', color: '#E34C26' },
-      { name: 'CSS', color: '#563D7C' },
-    ],
-    describe: '🎉 Web 端弹幕组件',
-    codeUrl: 'https://github.com/hai-zou/web-barrage',
-    demoUrl: 'https://hai-zou.github.io/web-barrage/',
-  },
-  {
-    id: nanoid(),
-    type: 'project',
-    previewImg: '/images/travel-preview.png',
-    name: 'Travel Record',
-    tags: [
-      { name: 'JavaScript', color: '#F1E05A' },
-      { name: 'HTML', color: '#E34C26' },
-    ],
-    describe: '🌍 记录每一个到过的地方！',
-    codeUrl: 'https://github.com/hai-zou/travel-record',
-    demoUrl: 'https://travel-record.luckyzh.cn',
-  },
-  {
-    id: nanoid(),
-    type: 'project',
     previewImg: '/images/blog-preview.png',
-    name: 'Hexo Blog',
+    name: 'Gholts Blog',
     tags: [
-      { name: 'JavaScript', color: '#F1E05A' },
-      { name: 'CSS', color: '#563D7C' },
-      { name: 'Nunjucks', color: '#3D8137' },
+      { name: 'Ruby', color: '#6D1F19' },
+      { name: 'HTML', color: '#DE5930' },
     ],
-    describe: 'Personal Blog 📝 | Front-end technology sharing',
-    codeUrl: 'https://github.com/hai-zou/hexo-blog',
-    demoUrl: 'https://www.luckyzh.cn',
+    describe: 'Personal Blog 📝 | General technology sharing',
+    codeUrl: 'https://github.com/Gholts/gholts-blog',
+    demoUrl: 'https://blog.gholts.top',
   },
   {
     id: nanoid(),
     type: 'user',
-    avatar: 'https://image.luckyzh.cn/user/avatar.jpg',
-    name: 'A Mortal',
+    avatar: 'https://avatars.githubusercontent.com/u/104642670?v=4',
+    name: 'Gholts',
     social: [
       {
         name: 'Location',
-        url: 'https://www.google.com/maps/place/ganzhou',
+        url: 'https://www.google.com/maps/place/China',
         icon: <MapPinIcon width={16} height={16} />,
         openNewPage: true,
       },
@@ -83,29 +51,11 @@ export const carouselList: Array<AllCard> = [
       },
       {
         name: 'Github',
-        url: 'https://github.com/hai-zou',
+        url: 'https://github.com/Gholts',
         icon: <GitHubIcon width={16} height={16} />,
         openNewPage: true,
       },
-      {
-        name: '掘金主页',
-        url: 'https://juejin.cn/user/202789929622183/posts',
-        icon: <JuejinIcon width={16} height={16} />,
-        openNewPage: true,
-      },
     ],
-    describe: 'Only by experiencing the hardest hardships can one rise above the ordinary.',
-  },
-  {
-    id: nanoid(),
-    type: 'custom',
-    name: 'mood',
-    children: <MoodCard />,
-  },
-  {
-    id: nanoid(),
-    type: 'custom',
-    name: 'clock',
-    children: <ClockCard />,
+    describe: '一個技術博客☝️🥸',
   },
 ];
